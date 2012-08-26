b67fd1334aeb13f164d7a4f9dcad1a5b920842a9
jeff.langr <jlangr@jlangr-ubuntu2.(none)>
2012-08-26 15:24:08 -0600

    x

--- PlaceDescriptionServiceTest.cpp
+++ PlaceDescriptionServiceTest.cpp
@@ -9,45 +9,42 @@
 using namespace std;
 using namespace testing;
 
+class HttpStub: public Http {
+public:
+   MOCK_CONST_METHOD1(get, string(const string&));
+};
+
 class APlaceDescriptionService: public Test {
 public:
    static const string ValidLatitude;
    static const string ValidLongitude;
+
+   HttpStub httpStub;
+   PlaceDescriptionService* service;
+
+   void SetUp() {
+      service = new PlaceDescriptionService(&httpStub);
+   }
+
+   void TearDown() {
+      delete service;
+   }
 };
 
 const string APlaceDescriptionService::ValidLatitude("38.005");
 const string APlaceDescriptionService::ValidLongitude("-104.44");
 
-// START:HttpStub
-class HttpStub: public Http {
-public:
-// START_HIGHLIGHT
-   MOCK_CONST_METHOD1(get, string(const string&));
-// END_HIGHLIGHT
-};
-// END:HttpStub
-
-// START:MakesHttpRequest
 TEST_F(APlaceDescriptionService, MakesHttpRequestToObtainAddress) {
-   HttpStub httpStub;
-   string urlStart(
-      "http://open.mapquestapi.com/nominatim/v1/reverse?format=json&");
-   string expectedURL = urlStart + 
-         "lat=" + APlaceDescriptionService::ValidLatitude + "&" +
-         "lon=" + APlaceDescriptionService::ValidLongitude;
-// START_HIGHLIGHT
+   string expectedURL(
+      string("http://open.mapquestapi.com/nominatim/v1/reverse?format=json&") +
+      "lat=" + APlaceDescriptionService::ValidLatitude + 
+      "&lon=" + APlaceDescriptionService::ValidLongitude);
    EXPECT_CALL(httpStub, get(expectedURL));
-// END_HIGHLIGHT
-   PlaceDescriptionService service(&httpStub);
 
-   service.summaryDescription(ValidLatitude, ValidLongitude);
+   service->summaryDescription(ValidLatitude, ValidLongitude);
 }
-// END:MakesHttpRequest
 
-// START:FormatsRetrievedAddress
 TEST_F(APlaceDescriptionService, FormatsRetrievedAddressIntoSummaryDescription) {
-   HttpStub httpStub;
-// START_HIGHLIGHT
    EXPECT_CALL(httpStub, get(_))
       .WillOnce(Return(
          stringutil::ticToQuote("{ 'address': {\
@@ -55,11 +52,9 @@ TEST_F(APlaceDescriptionService, FormatsRetrievedAddressIntoSummaryDescription) 
                                    'city':'Fountain',\
                                    'state':'CO',\
                                    'country':'US' }}")));
-// END_HIGHLIGHT
-   PlaceDescriptionService service(&httpStub);
 
-   string description = service.summaryDescription(ValidLatitude, ValidLongitude);
+   string description = service->summaryDescription(ValidLatitude, ValidLongitude);
 
    ASSERT_THAT(description, Eq("Drury Ln, Fountain, CO, US"));
 }
-// END:FormatsRetrievedAddress
+
