61fc8d8ca043a031e24dbc3db1c0d38cc9efba9e
jlangr <jeff@langrsoft.com>
2013-03-25 14:00:51 -0600

    fix

--- stringutil.h
+++ stringutil.h
@@ -2,11 +2,10 @@
 #define stringutil_h
 
 #include <string>
-#include <vector>
 #include <algorithm>
 
 namespace stringutil {
-   static std::string ticToQuote(const std::string& text) {
+   inline std::string ticToQuote(const std::string& text) {
       std::string copy(text);
       std::replace(copy.begin(), copy.end(), '\'', '"');
       return copy;
