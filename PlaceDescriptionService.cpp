23d325ecbb04732715a57da1cf9babf150f1c85b
jlangr <jeff@langrsoft.com>
2013-05-22 11:50:09 -0600

    7 was 8

--- PlaceDescriptionService.cpp
+++ PlaceDescriptionService.cpp
@@ -6,42 +6,12 @@
 
 using namespace std;
 
-// START:ctor
 PlaceDescriptionService::PlaceDescriptionService(Http* http) : http_(http) {}
-// END:ctor
 
-// START:createGetRequestUrl
+// START:emptySummaryDescription
 string PlaceDescriptionService::summaryDescription(
       const string& latitude, const string& longitude) const {
-   auto request = createGetRequestUrl(latitude, longitude);
-   auto response = get(request);
-   return summaryDescription(response);
+   return "";
 }
-
-string PlaceDescriptionService::summaryDescription(
-      const string& response) const {
-   AddressExtractor extractor;
-   auto address = extractor.addressFrom(response);
-   return address.summaryDescription();
-}
-
-string PlaceDescriptionService::get(const string& requestUrl) const {
-   return http_->get(requestUrl);
-}
-
-string PlaceDescriptionService::createGetRequestUrl(
-      const string& latitude, const string& longitude) const {
-   string server{"http://open.mapquestapi.com/"};
-   string document{"nominatim/v1/reverse"};
-   return server + document + "?" +
-      keyValue("format", "json") + "&" +
-      keyValue("lat", latitude) + "&" +
-      keyValue("lon", longitude);
-}
-
-string PlaceDescriptionService::keyValue(
-      const string& key, const string& value) const {
-   return key + "=" + value;
-}
-// END:createGetRequestUrl
+// END:emptySummaryDescription
 
--- PlaceDescriptionServiceTest.cpp
+++ PlaceDescriptionServiceTest.cpp
@@ -17,48 +17,29 @@ public:
 const string APlaceDescriptionService::ValidLatitude("38.005");
 const string APlaceDescriptionService::ValidLongitude("-104.44");
 
-// START:expectationURL
+// START:HttpStub
 class HttpStub: public Http {
 public:
-   string returnResponse;
 // START_HIGHLIGHT
-   string expectedURL;
+   MOCK_METHOD0(initialize, void());
+   MOCK_CONST_METHOD1(get, string(const string&));
 // END_HIGHLIGHT
-   void initialize() override {}
-   std::string get(const std::string& url) const override {
-      verify(url);
-      return returnResponse;
-   }
-
-   void verify(const string& url) const {
-// START_HIGHLIGHT
-      ASSERT_THAT(url, Eq(expectedURL));
-// END_HIGHLIGHT
-   }
 };
+// END:HttpStub
 
-TEST_F(APlaceDescriptionService, ReturnsDescriptionForValidLocation) {
+TEST_F(APlaceDescriptionService, MakesHttpRequestToObtainAddress) {
    HttpStub httpStub;
-   httpStub.returnResponse = // ...
-// END:expectationURL
-                             R"({"address": {
-                                    "road":"Drury Ln",
-                                    "city":"Fountain",
-                                    "state":"CO",
-                                    "country":"US" }})";
-// START:expectationURL
-// START_HIGHLIGHT
    string urlStart{
       "http://open.mapquestapi.com/nominatim/v1/reverse?format=json&"};
-   httpStub.expectedURL = urlStart + 
+   auto expectedURL = urlStart + 
       "lat=" + APlaceDescriptionService::ValidLatitude + "&" +
       "lon=" + APlaceDescriptionService::ValidLongitude;
+// START_HIGHLIGHT
+   EXPECT_CALL(httpStub, get(expectedURL));
 // END_HIGHLIGHT
    PlaceDescriptionService service{&httpStub};
 
-   auto description = service.summaryDescription(ValidLatitude, ValidLongitude);
-
-   ASSERT_THAT(description, Eq("Drury Ln, Fountain, CO, US"));
+   service.summaryDescription(ValidLatitude, ValidLongitude);
 }
 // END:expectationURL
 
