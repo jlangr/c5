8a9dcf29c4dc0ac190ba6b2a0f62275f9358d206
jlangr <jeff@langrsoft.com>
2013-05-22 18:01:26 -0600

    8 was 9

--- PlaceDescriptionService.cpp
+++ PlaceDescriptionService.cpp
@@ -8,10 +8,22 @@ using namespace std;
 
 PlaceDescriptionService::PlaceDescriptionService(Http* http) : http_(http) {}
 
-// START:emptySummaryDescription
+// START:summaryDescription
 string PlaceDescriptionService::summaryDescription(
       const string& latitude, const string& longitude) const {
+   string server{"http://open.mapquestapi.com/"};
+   string document{"nominatim/v1/reverse"};
+   string url = server + document + "?" +
+                keyValue("format", "json") + "&" +
+                keyValue("lat", latitude) + "&" +
+                keyValue("lon", longitude);
+   http_->get(url);
    return "";
 }
-// END:emptySummaryDescription
+// END:summaryDescription
+
+string PlaceDescriptionService::keyValue(
+      const string& key, const string& value) const {
+   return key + "=" + value;
+}
 
